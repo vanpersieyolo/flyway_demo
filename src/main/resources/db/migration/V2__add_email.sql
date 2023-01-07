c2aa2c2c19e126b01f10d41c42ecc7b2e881d954
vanda <ex-minhnhat001@ocft.com>
2023-01-07 13:56:46 +0700

    first commit

---
+++ src/main/resources/db/migration/V2__add_email.sql
@@ -0,0 +1,5 @@
+ALTER TABLE users ADD email VARCHAR(255);
+INSERT INTO users (username, password) VALUES ('user1', 'password1');
+INSERT INTO users (username, password) VALUES ('user2', 'password2');
+INSERT INTO users (username, password) VALUES ('user3', 'password3');
+
---
+++ src/test/java/com/example/demo_flyway/DemoFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFlywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
